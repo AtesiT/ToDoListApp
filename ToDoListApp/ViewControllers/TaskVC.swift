a929a85d62d6970c29f8588ff0a96274aca12bb0
AtesiT <atesit@mail.ru>
2026-02-03 18:50:32 +0300

    ADD: TaskViewController

---
+++ ToDoListApp/ViewControllers/TaskVC.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+final class TaskVC: UIViewController {
+
+    @IBOutlet var titleTask: UILabel!
+    @IBOutlet var dataTask: UILabel!
+    @IBOutlet var textTask: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
