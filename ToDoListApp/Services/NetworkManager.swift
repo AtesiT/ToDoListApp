f232a7598b2f1d63c8cfc726a2edfc952a25bf4a
AtesiT <atesit@mail.ru>
2026-02-06 16:49:28 +0300

    ADD: function saveInCoreData and updated fetchData func

--- ToDoListApp/Services/NetworkManager.swift
+++ ToDoListApp/Services/NetworkManager.swift
@@ -1,5 +1,6 @@
 import Foundation
-import CoreData
+import UIKit
+internal import CoreData
 
 final class NetworkManager {
     static let shared = NetworkManager()
@@ -7,9 +8,9 @@ final class NetworkManager {
     private init() {}
     
     //  Самая базовая функция для "забирания" данных из интернета
-    func fetchData(from url: URL, completion: @escaping (Result<ToDoList, Error>) -> Void) {
+    func fetchData(from url: URL) {
         //  Выполняем в другом потоке
-        DispatchQueue.global(qos: .userInitiated).async {
+        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
             URLSession.shared.dataTask(with: url) { data, _, error in
                 guard let data else {
                     print(error ?? "No error")
@@ -17,12 +18,38 @@ final class NetworkManager {
                 }
                 do {
                     let jsonData = try JSONDecoder().decode(ToDoList.self, from: data)
-                    completion(.success(jsonData))
+                    self?.saveInCoreData(todos: jsonData.todos)
                 } catch {
                     print(error)
-                    completion(.failure(error))
                 }
             }.resume()
+        }
+    }
+    
+    private func saveInCoreData(todos: [Todos]) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        //  Делаем так, чтобы на фоновом потоке выполнялись операции
+        let backgroundContext = appDelegate.persistentContainer.newBackgroundContext()
+        
+        backgroundContext.perform {
+            todos.forEach { todoData in
+                let task = TaskEntity(context: backgroundContext)
+                task.id = Int64(todoData.id)
+                task.todo = todoData.todo
+                task.completed = todoData.completed
+                task.userId = Int64(todoData.userId)
+            }
+            do {
+                try backgroundContext.save()
+                print("Проверка: \(todos.count)")
+                //  Отправляем уведомление с именем "DataLoaded"
+                DispatchQueue.main.async {
+                    NotificationCenter.default.post(name: NSNotification.Name("DataLoaded"), object: nil)
+                }
+            } catch {
+                print(error)
+            }
         }
     }
 }
